4da983f714371b42e99109c6a66505b052538b51
Sameed Hayat <sameedhayat@gmail.com>
2022-12-15 12:10:32 +0100

    Update main.tf

--- terraform/environments/test/main.tf
+++ terraform/environments/test/main.tf
@@ -18,11 +18,6 @@ module "resource_group" {
   resource_group       = "${var.resource_group}"
   location             = "${var.location}"
 }
-
-data "azurerm_resource_group" "test" {
-  name = "Azuredevops"
-}
-
 module "network" {
   source               = "../../modules/network"
   address_space        = "${var.address_space}"
@@ -30,7 +25,7 @@ module "network" {
   virtual_network_name = "${var.virtual_network_name}"
   application_type     = "${var.application_type}"
   resource_type        = "NET"
-  resource_group       = "${data.azurerm_resource_group.test.name}"
+  resource_group       = "${module.resource_group.resource_group_name}"
   address_prefix_test  = "${var.address_prefix_test}"
 }
 
@@ -39,7 +34,7 @@ module "nsg-test" {
   location         = "${var.location}"
   application_type = "${var.application_type}"
   resource_type    = "NSG"
-  resource_group   = "${data.azurerm_resource_group.test.name}"
+  resource_group   = "${module.resource_group.resource_group_name}"
   subnet_id        = "${module.network.subnet_id_test}"
   address_prefix_test = "${var.address_prefix_test}"
 }
@@ -48,14 +43,14 @@ module "appservice" {
   location         = "${var.location}"
   application_type = "${var.application_type}"
   resource_type    = "AppService"
-  resource_group   = "${data.azurerm_resource_group.test.name}"
+  resource_group   = "${module.resource_group.resource_group_name}"
 }
 module "publicip" {
   source           = "../../modules/publicip"
   location         = "${var.location}"
   application_type = "${var.application_type}"
   resource_type    = "publicip"
-  resource_group   = "${data.azurerm_resource_group.test.name}"
+  resource_group   = "${module.resource_group.resource_group_name}"
 }
 
 module "vm" {
@@ -63,7 +58,7 @@ module "vm" {
   location         = "${var.location}"
   application_type = "${var.application_type}"
   resource_type    = "vm"
-  resource_group   = "${data.azurerm_resource_group.test.name}"
+  resource_group   = "${module.resource_group.resource_group_name}"
   subnet_id        = "${module.network.subnet_id_test}"
   public_ip        = "${module.publicip.public_ip_address_id}"
 }
